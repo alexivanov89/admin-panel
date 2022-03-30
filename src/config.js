2513341afd9c7da5fd1d8fbb6dd48b856082ec5e
Alex Ivanov <a89.ivanov@yandex.ru>
2022-03-30 14:33:24 +0400

    F-08. Авторизация в админскую часть.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
 const API_URL = process.env.REACT_APP_API_URL;
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
-export { API_KEY, API_URL };
+export { API_KEY, API_URL, CLIENT_SECRET };
--- src/store/slices/authSlice.js
+++ src/store/slices/authSlice.js
@@ -1,13 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { authService } from '../../services/authService';
 
 const initialState = {
   isAuth: false,
-  login: {
+  user: {
     username: null,
     password: null,
   },
   isLoading: false,
-  isError: false,
+  errorAuth: null,
 };
 
 const authSlice = createSlice({
@@ -16,10 +17,37 @@ const authSlice = createSlice({
   reducers: {
     setIsAuth: (state, action) => {
       state.isAuth = action.payload;
+      state.isLoading = false;
+      state.errorAuth = null;
+    },
+    setUser: (state, action) => {
+      state.user = action.payload;
+      state.isLoading = false;
+      state.errorAuth = null;
+    },
+    setIsLoading: (state, action) => {
+      state.isLoading = action.payload;
+      state.errorAuth = null;
+    },
+    setErrorAuth: (state, action) => {
+      state.errorAuth = action.payload;
+      state.isLoading = false;
     },
   },
 });
 
-export const { setIsAuth } = authSlice.actions;
+export const login = (values) => async (dispatch) => {
+  try {
+    dispatch(setIsLoading(true));
+
+    const response = await authService.login(values);
+    dispatch(setIsAuth(true));
+    dispatch(setUser(values));
+  } catch (e) {
+    dispatch(setErrorAuth('Некорректный логин или пароль'));
+  }
+};
+
+export const { setIsAuth, setUser, setIsLoading, setErrorAuth } = authSlice.actions;
 
 export const authReducer = authSlice.reducer;
